22f22c6373c746351e743daeae51b4c2db713a2d
George Raptis <georapbox@gmail.com>
2015-11-09 18:19:31 +0200

    useCapture  option with default value to false

--- src/methods/on.js
+++ src/methods/on.js
@@ -4,9 +4,15 @@ module.exports = (function () {
     // Require dependencies
     var iterate = require('_iterator');
 
-    return function on(eventName, callback) {
+    /**
+     * Attach an event handler function for one event to the mathed elements.
+     * @param {string} eventName A string representing the event type to listen for.
+     * @param {function} listener A function to execute when the event is triggered.
+     * @param {boolean} [useCapture] If true, useCapture indicates that the user wishes to initiate capture. Default is false.
+     */
+    return function on(eventName, listener, useCapture ) {
         iterate(this, function (el) {
-            el.addEventListener(eventName, callback, false);
+            el.addEventListener(eventName, listener, useCapture || false);
         });
         return this;
     };
