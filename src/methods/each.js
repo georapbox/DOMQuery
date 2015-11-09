9ec37dde49b38483bf391577ae865ee9a88ec0fb
George Raptis <georapbox@gmail.com>
2015-11-09 16:23:22 +0200

    Provide th ability to stop loop of callback function

--- src/methods/each.js
+++ src/methods/each.js
@@ -1,13 +1,21 @@
 module.exports = (function () {
     'use strict';
 
-    // Require dependancies
-    var iterate = require('_iterator');
-    
-    return function each(callback) {
-        iterate(this, function (el, index) {
-            callback && callback(el, index);
-        });
+    /**
+     * Iterate over a DOMQuery object, executing a function for each matched element.
+     * @param {function} func A function to execute for each mathed element.
+     * @return {object} The DOMQuery object
+     */
+    return function each(func) {
+        var i = 0,
+            len = this.length;
+
+        for (i; i < len; i += 1) {
+            if (func && func(this[i], i) === false) {
+                return this;
+            }
+        }
+
         return this;
     };
 }());
