cb9608df0c27739a7f87ad31976594437239a08d
George Raptis <georapbox@gmail.com>
2015-11-09 14:07:40 +0200

    Provide the ability to remove more than one attribute at once with space-seperated string

--- src/attributes/removeAttr.js
+++ src/attributes/removeAttr.js
@@ -5,10 +5,20 @@ module.exports = (function () {
     var u = require('utils/index'),
         iterate = require('_iterator');
 
+    /**
+     * Remove one or more attributes from each element in the set of matched elements.
+     * @param {string} attrName The attribute(s) (space-seperated) to remove.
+     * @return {object} The DOMQuery object.
+     */
     return function attr(attrName) {
+        var attrs;
+
         if (attrName && u.is.string(attrName)) {
+            attrs = u.trim(attrName).split(' ');
             iterate(this, function (el) {
-                u.is.element(el) && el.removeAttribute(attrName);
+                attrs.forEach(function (attr) {
+                    u.is.element(el) && el.removeAttribute(attr);
+                });
             });
         }
         return this;
